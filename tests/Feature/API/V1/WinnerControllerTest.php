cd29a21adb56ffec109b60edf538a5be987dbafa
mohammad khodaei <mohammad19khodaei@gmail.com>
2020-12-20 19:56:51 +0330

    fix typo

--- tests/Feature/API/V1/WinnerControllerTest.php
+++ tests/Feature/API/V1/WinnerControllerTest.php
@@ -6,14 +6,13 @@ use Tests\TestCase;
 use App\Models\Winner;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class WinnerContrllerTest extends TestCase
+class WinnerControllerTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test */
     public function can_fetch_list_of_winners()
     {
-        $this->withoutExceptionHandling();
         $winners = Winner::factory()->count(5)->create();
         $winners = $winners->map(fn($winner) => $winner->only('phone', 'code'))->toArray();
 
